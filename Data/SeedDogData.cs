72b145bfc5d4b2eaf258c4afc03dd0de91aecc36
dlginsburg515 <davis.ginsburg@ndsu.due>
2024-12-09 12:34:48 -0600

    V1.0.5

---
+++ Data/SeedDogData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProject_DG_CH_BF.Models;
+
+namespace FinalProject_DG_CH_BF.Data
+{
+    public class SeedDogData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new FinalProject_DG_CH_BFContext (
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<FinalProject_DG_CH_BFContext>>());
+                   
+
+            if (context == null || context.Dog == null)
+            {
+                throw new NullReferenceException(
+                    "Null BlazorBookContext or Book DbSet");
+            }
+
+            if (context.Dog.Any())
+            {
+                return;
+            }
+
+            context.Dog.AddRange(
+                new Dog
+                {
+                    
+                },
+
+                new Dog
+                {
+                    
+                });
+
+            context.SaveChanges();
+        }
+
+    }
+}
--- Models/Dog.cs
+++ Models/Dog.cs
@@ -15,5 +15,10 @@ namespace FinalProject_DG_CH_BF.Models
         public string Gender { get; set; }
         public int Age {  get; set; }
         public string Description { get; set; }
+        public string HealthStatus { get; set; }
+        public Boolean GoodWithKids { get; set; }
+        public Boolean GoodWithDogs { get; set; }
+        public Boolean GoodWithCats { get; set; }
+
     }
 }
